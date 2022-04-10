acb408159bec61076f5e3a8638f225500da30a97
Erli <erli.elot@gmail.com>
2022-04-10 10:59:17 +0700

    Up Project

---
+++ untitled/src/Class/Home.java
@@ -0,0 +1,9 @@
+package Class;
+
+public class Home { // class
+
+    String names; // filed
+    int age;
+    final String country = "Indonesai"; // bila menggunkan final maka isi filed sudah tidak bisa dirubah lagi
+
+}
---
+++ untitled/src/Class/person.java
@@ -0,0 +1,23 @@
+package Class;
+
+public class person {
+    public static void main(String[] args) {
+
+        // cara membaut obeject
+
+        var Home = new Home();
+        var rumah = Home.age = 11;
+        // Home.country = "India"
+        Home.names = "Adi Waluyo";
+
+        Home home = new Home(); // cara buat object
+
+        Home home1;// cara membuat object
+        home1 = new Home();
+
+        // cara mengakses object
+        System.out.println(Home.names);
+        System.out.println(rumah);
+        System.out.println(home1.country);
+    }
+}
